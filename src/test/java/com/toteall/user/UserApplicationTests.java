fd845766ba1b590c9a045d29d1b7646bb22cef8f
iamjoydevdas <iamjoydevdas@gmail.com>
2025-04-17 21:47:25 +0100

    init commit

---
+++ src/test/java/com/toteall/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toteall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
