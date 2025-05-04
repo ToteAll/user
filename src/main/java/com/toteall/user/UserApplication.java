3004d7e8c768f7a427d4e90e9f5e500a1f853c0c
iamjoydevdas <iamjoydevdas@gmail.com>
2025-05-04 11:41:24 +0100

    clean up applications

--- src/main/java/com/toteall/user/UserApplication.java
+++ src/main/java/com/toteall/user/UserApplication.java
@@ -1,9 +1,6 @@
 package com.toteall.user;
 
-import com.toteall.user.repo.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
