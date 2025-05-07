3ad2f2afb499a5a467654ab2821b1393cb03f9a8
iamjoydevdas <iamjoydevdas@gmail.com>
2025-05-07 23:05:17 +0100

    added new api, context and cross origin

--- src/main/java/com/toteall/user/controller/UserController.java
+++ src/main/java/com/toteall/user/controller/UserController.java
@@ -3,11 +3,14 @@ package com.toteall.user.controller;
 import com.toteall.user.dto.Users;
 import com.toteall.user.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
@@ -16,5 +19,10 @@ public class UserController {
     @GetMapping("/user")
     public List<Users> user() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{userId}")
+    public String user(@PathVariable Long userId) {
+        return userRepository.findById(userId).get().getUsername();
     }
 }
